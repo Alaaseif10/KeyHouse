c13f8bb7557590b6c9561dfbef8177cd2cb29bf5
Alaaseif10 <101071489+Alaaseif10@users.noreply.github.com>
2024-09-08 22:55:45 +0300

    Add project files.

---
+++ Controllers/KeyHouseDB.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KeyHouse.Controllers
+{
+    public class KeyHouseDB : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
